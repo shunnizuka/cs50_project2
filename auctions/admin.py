a83a56f8853a440b352ccac90954940eafe80efe
shunnizuka <e0201222@u.nus.edu>
2020-08-26 22:47:52 +0800

    Update models + create admin page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bids, Category, Listing, WatchList, Comments
 
 # Register your models here.
+admin.site.register(Bids)
+admin.site.register(Category)
+admin.site.register(Listing)
+admin.site.register(WatchList)
+admin.site.register(Comments)
--- auctions/models.py
+++ auctions/models.py
@@ -5,37 +5,38 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Listing(models.Model):
-    title = models.CharField()
-    description = models.CharField()
-    active = models.BooleanField()
-    imageUrl = models.CharField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Lister")
-    bid = models.ForeignKey(Bids, on_delete=models.CASCADE, related_name="highestBid") 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listingCategory")
-
-    def __str__(self):
-        return f"{self.title}: {self.description}, Lister: {self.user}"
-
 class Bids(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return f"{self.user}: {self.price}"
 
 class Comments(models.Model):
-    comment = models.CharField()
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listingComment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    comment = models.CharField(max_length=255)
 
     def __str__(self):
-        return f"{self.listing}: {self.comment}"
+        return f"{self.user}: {self.comment}"
 
 class Category(models.Model):
-    category = models.CharField()
+    category = models.CharField(max_length=64)
 
     def __str__(self):
         return f"{self.category}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    active = models.BooleanField()
+    imageUrl = models.CharField(max_length=255, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Lister")
+    bid = models.ForeignKey(Bids, on_delete=models.CASCADE, related_name="highestBid") 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, related_name="listingCategory")
+    comments = models.ManyToManyField(Comments, blank=True, related_name="ListingComments")
+
+    def __str__(self):
+        return f"{self.title}: {self.description}, Lister: {self.user}"
 
 class WatchList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="WatchlistUser")
